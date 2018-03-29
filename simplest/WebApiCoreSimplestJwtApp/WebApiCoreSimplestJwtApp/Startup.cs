c573a7da0c19389e6f9a0099cd628e9d496da16d
Foyzul Karim <foyzulkarim@gmail.com>
2018-03-30 01:02:18 +0600

    startup configured for validation

---
+++ simplest/WebApiCoreSimplestJwtApp/WebApiCoreSimplestJwtApp/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebApiCoreSimplestJwtApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // configure how to validate the jwt token 
+            string bizbook365 = "http://bizbook365.com";
+            var validationParameter = new TokenValidationParameters()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+
+                ValidIssuer = bizbook365,
+                ValidAudience = bizbook365,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(bizbook365))
+            };
+
+            services.AddAuthentication("Bearer").AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = validationParameter;
+                });
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
