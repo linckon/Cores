b05e9cc0e7956b64817097465e9ca2344eaa468a
Foyzul Karim <foyzulkarim@gmail.com>
2018-03-30 15:42:22 +0600

    added new WebApiCoreJwtAppWithDb sln

---
+++ simplest/WebApiCoreJwtAppWithDb/WebApiCoreJwtAppWithDb/Controllers/ValuesController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiCoreJwtAppWithDb.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [Authorize]
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            string name = User.Identity.Name;
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ simplest/WebApiCoreJwtAppWithDb/WebApiCoreJwtAppWithDb/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebApiCoreJwtAppWithDb
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // configure how to validate the jwt token 
+            string bizbook365 = "http://bizbook365.com";
+            var validationParameter = new TokenValidationParameters()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+
+                ValidIssuer = bizbook365,
+                ValidAudience = bizbook365,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(bizbook365))
+            };
+
+            services.AddAuthentication("Bearer").AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = validationParameter;
+                });
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
